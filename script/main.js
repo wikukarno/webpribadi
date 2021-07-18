750de55a576e7e89bcfb80c2a7efa5a2244f957b
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-07-18 22:05:49 +0700

    update file & maintence

---
+++ script/main.js
@@ -0,0 +1,12 @@
+// to top
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 600) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+});
+
+// End to top
